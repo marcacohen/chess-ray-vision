97001b8d281d298ad60e515b460bd5c3bb649fa4
marcacohen <marcacohen@gmail.com>
2021-01-24 17:59:43 +0000

    tweak

--- src/pytorch_general/tensorboard_helper.py
+++ src/pytorch_general/tensorboard_helper.py
@@ -2,8 +2,7 @@
 
 # AND THIS https://github.com/yunjey/pytorch-tutorial/blob/master/tutorials/04-utils/tensorboard/main.py
 import tensorflow as tf
-import tf.compat.v1.summary as summary
-import tf.compat.v1.summary.Summary as Summary
+import tensorflow.compat.v1.summary as summary
 import numpy as np
 import scipy.misc 
 try:
@@ -20,7 +19,7 @@ class Logger(object):
 
     def scalar_summary(self, tag, value, step):
         """Log a scalar variable."""
-        summary = Summary(value=[Summary.Value(tag=tag, simple_value=value)])
+        summary = summary.Summary(value=[summary.Summary.Value(tag=tag, simple_value=value)])
         self.writer.add_summary(summary, step)
 
     def image_summary(self, tag, images, step):
@@ -36,14 +35,14 @@ class Logger(object):
             scipy.misc.toimage(img).save(s, format="png")
 
             # Create an Image object
-            img_sum = Summary.Image(encoded_image_string=s.getvalue(),
+            img_sum = summary.Summary.Image(encoded_image_string=s.getvalue(),
                                        height=img.shape[0],
                                        width=img.shape[1])
             # Create a Summary value
-            img_summaries.append(Summary.Value(tag='%s/%d' % (tag, i), image=img_sum))
+            img_summaries.append(summary.Summary.Value(tag='%s/%d' % (tag, i), image=img_sum))
 
         # Create and write Summary
-        summary = Summary(value=img_summaries)
+        summary = summary.Summary(value=img_summaries)
         self.writer.add_summary(summary, step)
         
     def histo_summary(self, tag, values, step, bins=1000):
@@ -70,6 +69,6 @@ class Logger(object):
             hist.bucket.append(c)
 
         # Create and write Summary
-        summary = Summary(value=[Summary.Value(tag=tag, histo=hist)])
+        summary = summary.Summary(value=[summary.Summary.Value(tag=tag, histo=hist)])
         self.writer.add_summary(summary, step)
         self.writer.flush()
