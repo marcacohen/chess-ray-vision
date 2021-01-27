ff27d239cad29b0a73ad8d4fdfd9b58241712d2b
marcacohen <marcacohen@gmail.com>
2021-01-27 10:56:40 +0000

    tweak

--- src/pytorch_general/tensorboard_helper.py
+++ src/pytorch_general/tensorboard_helper.py
@@ -19,7 +19,6 @@ class Logger(object):
     def scalar_summary(self, tag, value, step):
         """Log a scalar variable."""
 
-        return
         with self.writer.as_default():
             tf.summary.scalar(tag, value, step=step)
             self.writer.flush()
